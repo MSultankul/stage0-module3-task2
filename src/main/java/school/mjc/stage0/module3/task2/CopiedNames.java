fa0ffa8e936b241e8e45a18c4411b1a262167221
Moldashbek Sultankul <sultankulmaksatuly@gmail.com>
2023-09-19 20:25:30 +0600

    vfvdf

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -2,13 +2,22 @@ package school.mjc.stage0.module3.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
+
+        int variable1 = 0;
+
         int variable2 = 2000;
-        int value = 1000;
+
+        int value1 = 1000;
         int variable3 = 4000;
+
         int value2 = 3000;
-        int variables4 = 5000;
+
+        int variables = 5000;
         variables = 7000;
         int value3 = 90000;
+        int myInteger = 100000;
+        int myInteger2 = 1000000;
+        int myVar = 1234567;
+        int myInteger3 = 1234567;
     }
 }
